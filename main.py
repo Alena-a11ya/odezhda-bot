ae731b4092a65728149858594927400542488c98
Alena-a11ya <a83755097@gmail.com>
2026-03-23 15:44:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 text = update.message.text.lower()
 
 
-   elif  lower_message in ["🕒 режим работы", "режим работы", "график"]:
+   if  lower_message in ["🕒 режим работы", "режим работы", "график"]:
         answer = "🕒 Ежедневно с 10:00 до 18:00"
         await update.message.reply_text(answer, reply_markup=reply_markup)
         chat_memory[chat_id].append({"role": "user", "content": user_message})
